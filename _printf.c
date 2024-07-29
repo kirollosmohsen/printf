1bb953dc622b4cf0fc4461bccfeefaf769ec43ee
Ahmed <ahmed.shabasy16@gmail.com>
2024-07-29 03:10:41 -0700

    edit

--- _printf.c
+++ _printf.c
@@ -23,7 +23,7 @@ int _printf(const char * const format, ...)
 Here:
 	while (format[i] != '\0')
 	{
-		j = 13;
+		j = 2;
 		while (j >= 0)
 		{
 			if (p[j].ph[0] == format[i] && p[j].ph[1] == format[i + 1])
